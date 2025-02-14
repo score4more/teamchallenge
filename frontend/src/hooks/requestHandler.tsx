e70baa93cac6267072dc0f7b2d511bb85d8403ae
d506444 <bassem.boutros@burda-forward.de>
2025-02-14 16:26:39 +0100

    Logout if token expires

--- frontend/src/hooks/requestHandler.tsx
+++ frontend/src/hooks/requestHandler.tsx
@@ -1,3 +1,5 @@
+import {getGlobalLogout} from "../context/AuthContext.js";
+
 export const privateRequest = async (url: string, token: string, options: RequestInit = {}) => {
   if (!token) {
     throw new Error("No authentication token found. Please log in.");
@@ -6,8 +8,12 @@ export const privateRequest = async (url: string, token: string, options: Reques
   const headers = new Headers(options.headers || {});
   headers.set("Authorization", `Bearer ${token}`);
 
-  return fetch(url, {
-    ...options,
-    headers,
+  return fetch(url, { ...options, headers }).then((response) => {
+    if (response.status === 401) {
+      const logout = getGlobalLogout();
+      if (logout) logout();
+      throw new Error("Unauthorized. Logging out...");
+    }
+    return response;
   });
 };
